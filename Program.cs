b0c19e7e1e4feb0f6067e983872d90bf51e01677
Caleb <calebcsanchez1@gmail.com>
2019-08-29 13:30:43 -0400

    Created PressToContinue

--- Program.cs
+++ Program.cs
@@ -2,11 +2,20 @@
 
 namespace DefaultForVSCode
 {
+    
     class Program
     {
+        
+        static void PressToContinue()         //Created to allow fast executions to be seen on Console
+
+        {
+            Console.WriteLine("Press any key to continue.");
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            PressToContinue();
         }
     }
 }
